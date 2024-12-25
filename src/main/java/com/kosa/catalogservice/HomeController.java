6388fe75f3239583abcd1fbfd555419f7b167a51
masungil <masungil2@daum.net>
2024-12-25 17:45:22 +0900

    사용자 지정 설정 (config) 관련 기능 구현 하기 환영메시지를 설정 파일로 분리하여 구현 하기 방법

--- src/main/java/com/kosa/catalogservice/HomeController.java
+++ src/main/java/com/kosa/catalogservice/HomeController.java
@@ -3,11 +3,22 @@ package com.kosa.catalogservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kosa.catalogservice.config.PolarProperties;
+
 @RestController
 public class HomeController {
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "도서 카달로그에 오신 것을 환영합니다";
+        // return "도서 카달로그에 오신 것을 환영합니다";
+        // 설정 데이터 빈에서 가져온 환영 메시지를 사용함
+        return polarProperties.getGreeting();
+
     }
 
 }
