f9c7a5b8d03997f3bac82a169f7e44cafe759020
masungil <masungil2@daum.net>
2024-12-25 18:34:32 +0900

    외부화된 구성을 사용하여 설정 변수 설정하고 실행 하는 방법

--- src/main/java/com/kosa/catalogservice/config/PolarProperties.java
+++ src/main/java/com/kosa/catalogservice/config/PolarProperties.java
@@ -15,3 +15,17 @@ public class PolarProperties {
     }
 
 }
+
+/*
+ * 외부화된 구성 : 하나의 빌드로 여러 설정을 변경 하여 실행 할 수 있음
+ * 
+ * polar.greeting 속성값 선택단계 및 방법
+ * 1. CLI 인수로 설정 : java -jar *.jar --polar.greeting="속성값 설정"
+ * 2. JVM 인수로 설정 : java -jar -Dpolar.greeting="속성값 설정"*.jar
+ * 3. 환경 변수로 설정 :
+ * export POLAR_GREETING=속성값 설정
+ * java -jar *.jar
+ * 환경 변수명 POLAR_GREETING은 자바에서 속성으로 읽을 때는 polar.greeting으로 변환됨 (이런 것은 완화된
+ * 바이딩이라고함)
+ * 
+ */
