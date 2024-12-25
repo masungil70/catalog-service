abdc411f4c454efe480364917f42f4acc78c7c2a
masungil <masungil2@daum.net>
2024-12-25 11:52:15 +0900

    @JsonTest를 사용한 JSON 직렬화 테스트

---
+++ src/test/java/com/kosa/catalogservice/web/BookJsonTests.java
@@ -0,0 +1,44 @@
+package com.kosa.catalogservice.web;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import com.kosa.catalogservice.domain.Book;
+
+@JsonTest
+public class BookJsonTests {
+    @Autowired
+    private JacksonTester<Book> json;
+
+    @Test
+    void testSerialize() throws Exception {
+        var book = new Book("1234567890", "책 제목", "책 저자", 9.90);
+        var jsonContent = json.write(book);
+        assertThat(jsonContent).extractingJsonPathStringValue("@.isbn").isEqualTo(book.isbn());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.title").isEqualTo(book.title());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.author").isEqualTo(book.author());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.price").isEqualTo(book.price());
+        assertThat(jsonContent).extractingJsonPathStringValue("$.isbn").isEqualTo(book.isbn());
+        assertThat(jsonContent).extractingJsonPathStringValue("$.title").isEqualTo(book.title());
+        assertThat(jsonContent).extractingJsonPathStringValue("$.author").isEqualTo(book.author());
+        assertThat(jsonContent).extractingJsonPathNumberValue("$.price").isEqualTo(book.price());
+    }
+
+    @Test
+    void testDeserialize() throws Exception {
+        var content = """
+                    {
+                        "isbn": "1234567890",
+                        "title": "책 제목",
+                        "author": "책 저자",
+                        "price": 9.90
+                    }
+                """;
+        assertThat(json.parse(content))
+                .usingRecursiveComparison()
+                .isEqualTo(new Book("1234567890", "책 제목", "책 저자", 9.90));
+    }
+}
