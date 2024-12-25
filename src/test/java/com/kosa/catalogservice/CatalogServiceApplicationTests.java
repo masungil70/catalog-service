dc7b0bb38080d660c7b9682eb5848e10c2d63085
masungil <masungil2@daum.net>
2024-12-25 09:30:36 +0900

    @SpringBootTest를 위한 통합테스트를 설정 및 구현

--- src/test/java/com/kosa/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/kosa/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,33 @@
 package com.kosa.catalogservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.kosa.catalogservice.domain.Book;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void whenPostRequestThenBookCreated() {
+		var expectedBook = new Book("1234567891", "책 제목", "책 저자", 9.90);
+
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook)
+				.exchange() // url을 요청한다
+				.expectStatus().isCreated() // 응답 코드가 201번(HttpStatus.CREATED) 인지 확인한다
+				.expectBody(Book.class).value(actualBook -> {
+					assertThat(actualBook).isNotNull(); // http 응답의 본문이 null값이 아닌지 확인한다
+					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn()); // 실제객체의 isbn과 예상 isbn의 값이 같은지 확인한다
+				});
 	}
 
 }
