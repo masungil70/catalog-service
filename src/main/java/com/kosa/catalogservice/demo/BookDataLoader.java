ac14cbd7a884b810c077a891f4b3f9f6971faeec
masungil <masungil2@daum.net>
2024-12-25 18:04:49 +0900

    프로파일 기능 플래그를 설정하여 개발시 초기 데이터를 로딩하여 사용할 수 있게 기능 구현

---
+++ src/main/java/com/kosa/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,25 @@
+package com.kosa.catalogservice.demo;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.kosa.catalogservice.domain.Book;
+import com.kosa.catalogservice.domain.BookRepository;
+
+@Component
+@Profile("testdata")
+public class BookDataLoader {
+    private final BookRepository bookRepository;
+
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        bookRepository.save(new Book("1234567891", "책 제목1", "책 저자1", 9.90));
+        bookRepository.save(new Book("1234567892", "책 제목2", "책 저자2", 19.90));
+    }
+}
