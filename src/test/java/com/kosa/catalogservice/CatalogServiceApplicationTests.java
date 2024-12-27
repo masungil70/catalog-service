385c65e229527f6034d7d29ffc01cb8b5554f554
masungil <masungil2@daum.net>
2024-12-27 09:47:32 +0900

    @SpringBootTest 및 테스트컨테이너를 이용한 통합 테스트 1. @SpringBootTest 어너테이션을 통해 완전한 통합 테스트 2. @ActiveProfiles 어너테이션을 통한 활성화된 프로파일 설정 3. 마리아 DB에서 실행되는 명령어 로그을 출력 하기 위한 설정   SHOW VARIABLES LIKE '%general_log%';   위 쿼리 실행 결과  general_log_file 필드의 값을 확인하고 해당 파일을 찾아  리눅스의 경우 tail -f 파일명으로 실행하여 로그를 확인한다   로그 활성화   SET GLOBAL general_log = ON;   로그 비활성화   SET GLOBAL general_log = OFF;  로그 활성화는 테스트 시 확인을 하는 목적으로  실행하는 것이 좋다(계속 활성화 상태이면 저장 공간을 너무 많이 사용한다)

--- src/test/java/com/kosa/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/kosa/catalogservice/CatalogServiceApplicationTests.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.kosa.catalogservice.domain.Book;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("intergration")
 class CatalogServiceApplicationTests {
 
 	@Autowired
